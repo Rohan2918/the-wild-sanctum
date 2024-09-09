9935c7011a55f4e7432d62e2e92157ba4b80e73d
Rohan Avhad <97187192+Rohan2918@users.noreply.github.com>
2024-09-09 14:55:12 +0530

    supabase.js

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,8 +1,8 @@
 
 import { createClient } from '@supabase/supabase-js';
 
-export const supabaseUrl = 'https://odwcykjmcbeqayxjmxhv.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9kd2N5a2ptY2JlcWF5eGpteGh2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTgxMTY0MDMsImV4cCI6MjAzMzY5MjQwM30.5henQS0jIRzJJtC8om3Pw-tYUP0b-vv7TeAFEdfHOGI';
+export const supabaseUrl = '[supabase_url]'
+const supabaseKey = '[your_supabase_key]'
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 export default supabase;
